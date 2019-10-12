d5c4226f4d2198d4a346cd5e0bbc194330a21320
bensisko69 <loicrg69@gmail.com>
2019-10-12 18:29:57 +0200

    config mysql

--- projectMAgic/settings.py
+++ projectMAgic/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'magicApp',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,13 @@ WSGI_APPLICATION = 'projectMAgic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'magic',
+        'USER' : 'bensisko',
+        'PASSWORD' : '18041986.'
+        'HOST' : 'bensisko.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
